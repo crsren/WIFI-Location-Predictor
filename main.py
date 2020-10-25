aef44d815cc8581b92da134fceb764b5c839eec9
Chris <chris.renschler@gmail.com>
2020-10-25 12:18:57 +0000

    Init main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+clean_ds = np.loadtxt("wifi_db/clean_dataset.txt")
+noisy_ds = np.loadtxt("wifi_db/noisy_dataset.txt")
+
+#print(noisy_ds)
+print(clean_ds)
+
+node = {'attribute', 'value', 'left', 'right'}
+
+def decision_tree_learning(ds, depth):
+    if(true):
+        print("hi")
+
+
+def find_split(ds):
+    return
