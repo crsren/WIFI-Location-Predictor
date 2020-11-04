5871d949334a0770f9439ae771bcfcdcda6a9144
nayankad <nk2018@ic.ac.uk>
2020-11-04 23:53:03 +0000

    bare stuff

--- main.py
+++ main.py
@@ -6,11 +6,24 @@ from node import Node
 from evaluate import *
 from prune import *
 
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
+from collections import deque
+
 def loadClean():
     return np.loadtxt("wifi_db/clean_dataset.txt")
 
 def loadNoisy():
     return np.loadtxt("wifi_db/noisy_dataset.txt")
+
+
+
+
+
+
 
 def main():
     np.random.seed(100)
@@ -35,11 +48,51 @@ def main():
 
     avgAccuracy = crossValidate(ds)
     print("average accuracy: ", avgAccuracy)
-    prune(root)
+    #prune(root, testSet)
+
+    fig, ax = plt.subplots(figsize=(18, 10))
+    tree, depth, leafCount = decision_tree_learning(trainingSet)
+    prune(tree, testSet)
+    gap = 1.0/depth
+    plot_graph(tree, 0.0, 1.0, 0.0, 1.0, gap, ax)
+    fig.subplots_adjust(top=0.98)
+    fig.subplots_adjust(bottom=0.03)
+    fig.subplots_adjust(left=0.03)
+    fig.subplots_adjust(right=0.99)
+    plt.show()
 
     return
 
 
 
+
 if __name__ == '__main__':
         main()
+
+def plot_graph(root, xmin, xmax, ymin, ymax, gap, ax):
+    queue = deque([(root, xmin, xmax, ymin, ymax)])
+    while len(queue) > 0:
+       q = queue.popleft()
+       node = q[0]
+       xmin = q[1]
+       xmax = q[2]
+       ymin = q[3]
+       ymax = q[4]
+       atri = node.attribute
+       val = node.value
+       text = '['+str(atri)+']:'+str(val)
+
+       center = xmin+(xmax-xmin)/2.0
+       d = (center-xmin)/2.0
+
+       if node.left != None:
+         queue.append((node.left, xmin, center, ymin, ymax-gap))
+         ax.annotate(text, xy=(center-d, ymax-gap), xytext=(center, ymax),arrowprops=dict(arrowstyle="->"),)
+
+       if node.right != None:
+         queue.append((node.right, center, xmax, ymin, ymax-gap))
+         ax.annotate(text, xy=(center+d, ymax-gap), xytext=(center, ymax),arrowprops=dict(arrowstyle="->"),)
+
+       if node.left is None and node.right is None:
+         an1 = ax.annotate(node.value, xy=(center, ymax), xycoords="data", va="bottom", ha="center",
+                           bbox=dict(boxstyle="round", fc="w"))
--- prune.py
+++ prune.py
@@ -1,19 +1,32 @@
 from node import Node
 from evaluate import *
+import copy
+from build import *
 
-def prune(node):
 
-    save_tree = node.tree_copy()
-    pre_accuracy = evaluate(dataset, node)
 
-    if (node.right.leafornot is False):
-        prune(node.right)
-    elif (node.left.leafornot is False):
-        prune(node.left)
+#save_tree = root.tree_copy()
+#prunes = []
+
+def prune(node, test_data):
+
+    pre_accuracy = evaluate(test_data, node)
+
+    if (node.left.leaf is False):
+        prune(node.left, test_data)
+    elif (node.right.leaf is False):
+        prune(node.right, test_data)
     else:
-        print(node)
+        tempNode = copy.deepcopy(node)
+        # tempNode = Node(node.attribute, node.value, node.left, node.right, node.leaf, node.dataset)
+        # tmp.attribute = node.attribute
+        # tmp.value = node.value
+        # tmp.left = node.left
+        # tmp.right = node.right
+        # tmp.leaf = node.leaf
+        # tmp.dataset = node.dataset
 
-        if(node.left.ds.size() > node.right.ds.size()):
+        if(len(node.left.dataset) > len(node.right.dataset)):
             node.value = node.left.value
             node.attribute = node.left.attribute
         else:
@@ -22,15 +35,24 @@ def prune(node):
 
         node.left = None
         node.right = None
+        node.leaf = True
 
-        post_accuracy = evaluate(dataset, node)
+        post_accuracy = evaluate(test_data, node)
 
         if(post_accuracy >= pre_accuracy):
             #keep prune
             print("keep prune")
+            return node
         else:
-            #restore leaves
+            #restore leaves - need stack to store prunes prior to accuracy check?
             print("restore leaves")
+            node.attribute = tempNode.attribute
+            node.value = tempNode.value
+            node.left = tempNode.left
+            node.right = tempNode.right
+            node.leaf = tempNode.leaf
+            node.dataset = tempNode.dataset
+            return node
 
 
         #remove node (turn into leaf with side of higher number samples)
