235d98afd461595438d3798757eafa1208ac4677
Chris <chris.renschler@gmail.com>
2020-11-05 18:05:11 +0000

    Tried plotting the tree
    SB clean this up please?

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import numpy as np
 from __init__ import *
 
-# import matplotlib.pyplot as plt
-# import matplotlib.path as mpath
-# import matplotlib.lines as mlines
-# import matplotlib.patches as mpatches
-# from matplotlib.collections import PatchCollection
-# from collections import deque
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
+from collections import deque
+
+
+def plot_graph(root, xmin, xmax, ymin, ymax, gap, ax):
+    queue = deque([(root, xmin, xmax, ymin, ymax)])
+    while len(queue) > 0:
+        q = queue.popleft()
+        node = q[0]
+        xmin = q[1]
+        xmax = q[2]
+        ymin = q[3]
+        ymax = q[4]
+        atri = node.attribute
+        val = node.value
+        text = '['+str(atri)+']:'+str(val)
+
+        center = xmin+(xmax-xmin)/2.0
+        d = (center-xmin)/2.0
+
+        if node.left != None:
+            queue.append((node.left, xmin, center, ymin, ymax-gap))
+            ax.annotate(text, xy=(center-d, ymax-gap),
+                        xytext=(center, ymax), arrowprops=dict(arrowstyle="->"),)
+
+        if node.right != None:
+            queue.append((node.right, center, xmax, ymin, ymax-gap))
+            ax.annotate(text, xy=(center+d, ymax-gap),
+                        xytext=(center, ymax), arrowprops=dict(arrowstyle="->"),)
+
+        if node.left is None and node.right is None:
+            an1 = ax.annotate(node.leaf, xy=(center, ymax), xycoords="data", va="bottom", ha="center",
+                              bbox=dict(boxstyle="round", fc="w"))
 
 
 def loadClean():
@@ -41,56 +72,21 @@ def main():
     # Split into actual validation set later!!!
     root.perfectlyPruned(testSet, root)
 
-    return root, testSet
-
     # avgAccuracy = crossValidate(ds)
     # print("average accuracy: ", avgAccuracy)
     #prune(root, testSet)
 
-    # fig, ax = plt.subplots(figsize=(18, 10))
-    # tree, depth, leafCount = decision_tree_learning(trainingSet)
-    # prune(tree, tree, testSet)
-    # gap = 1.0/depth
-    # plot_graph(tree, 0.0, 1.0, 0.0, 1.0, gap, ax)
-    # fig.subplots_adjust(top=0.98)
-    # fig.subplots_adjust(bottom=0.03)
-    # fig.subplots_adjust(left=0.03)
-    # fig.subplots_adjust(right=0.99)
-    # plt.show()
+    fig, ax = plt.subplots(figsize=(18, 10))
+    gap = 1.0/depth
+    plot_graph(root, 0.0, 1.0, 0.0, 1.0, gap, ax)
+    fig.subplots_adjust(top=0.98)
+    fig.subplots_adjust(bottom=0.03)
+    fig.subplots_adjust(left=0.03)
+    fig.subplots_adjust(right=0.99)
+    plt.show()
 
-    # return
+    return root, testSet
 
 
 if __name__ == '__main__':
     main()
-
-
-# def plot_graph(root, xmin, xmax, ymin, ymax, gap, ax):
-#     queue = deque([(root, xmin, xmax, ymin, ymax)])
-#     while len(queue) > 0:
-#         q = queue.popleft()
-#         node = q[0]
-#         xmin = q[1]
-#         xmax = q[2]
-#         ymin = q[3]
-#         ymax = q[4]
-#         atri = node.attribute
-#         val = node.value
-#         text = '['+str(atri)+']:'+str(val)
-
-#         center = xmin+(xmax-xmin)/2.0
-#         d = (center-xmin)/2.0
-
-#         if node.left != None:
-#             queue.append((node.left, xmin, center, ymin, ymax-gap))
-#             ax.annotate(text, xy=(center-d, ymax-gap),
-#                         xytext=(center, ymax), arrowprops=dict(arrowstyle="->"),)
-
-#         if node.right != None:
-#             queue.append((node.right, center, xmax, ymin, ymax-gap))
-#             ax.annotate(text, xy=(center+d, ymax-gap),
-#                         xytext=(center, ymax), arrowprops=dict(arrowstyle="->"),)
-
-#         if node.left is None and node.right is None:
-#             an1 = ax.annotate(node.value, xy=(center, ymax), xycoords="data", va="bottom", ha="center",
-#                               bbox=dict(boxstyle="round", fc="w"))
