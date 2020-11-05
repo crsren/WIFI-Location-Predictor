44ab134f6d83859eea44157957ec425179c7c58c
Chris <chris.renschler@gmail.com>
2020-11-05 15:03:00 +0000

    (Hopefully) dealt with imports

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from node import Node
+from build import *
+from evaluate import *
+from prune import *
--- evaluate.py
+++ evaluate.py
@@ -1,14 +1,12 @@
 import numpy as np
+from __init__ import *
 
-from node import Node
-from build import decision_tree_learning
 
-# function to predict 
-def predict(node, signals):
+def predict(node, signals):  # function to predict
 
     if(node.leaf):
-        return node.value
-    
+        return node.leaf
+
     if(signals[node.attribute] > node.value):
         # print(node.attribute,": ", signals[node.attribute], " > ", node.value)
         return predict(node.right, signals)
@@ -16,49 +14,47 @@ def predict(node, signals):
         # print(node.attribute,": ", signals[node.attribute], " < ", node.value)
         return predict(node.left, signals)
 
-# return accuracy
-def evaluate(ds, tree):
+
+def evaluate(ds, tree):  # return accuracy
     correct = 0
 
     for dp in ds:
         print(dp)
-        print("Actual: ", dp[7], " | Predicted: ", predict(tree,dp))
+        print("Actual: ", dp[7], " | Predicted: ", predict(tree, dp))
         if(predict(tree, dp) == dp[7]):
-            correct+=1
-    
+            correct += 1
+
     return correct / len(ds)
 
-# returns confusion matrix
-def confusionMatrix(ds, tree):
-    confusion = np.zeros([4][4]) #actual, predicted
+
+def confusionMatrix(ds, tree):  # returns confusion matrix
+    confusion = np.zeros([4][4])  # actual, predicted
 
     for dp in ds:
-        print("(CM) Actual: ", ds[7], " | Predicted: ", predict(tree,dp))
-        confusion[ds[7]][predict(tree,dp)]+=1
-    
+        print("(CM) Actual: ", ds[7], " | Predicted: ", predict(tree, dp))
+        confusion[ds[7]][predict(tree, dp)] += 1
+
     return confusion
 
-#def plotCM(cm):
-    #TODO
+# def plotCM(cm):
+    # TODO
 
 
 def crossValidate(ds, k=10):
-    np.random.shuffle(ds) # just in case this hasn't been done before
-    folds = np.split(ds,k)
+    np.random.shuffle(ds)  # just in case this hasn't been done before
+    folds = np.split(ds, k)
     accuracy = 0
 
-    for i in range (0,k):
-        #use i as test set and !i as training set
+    for i in range(0, k):
+        # use i as test set and !i as training set
         testSet = folds[i]
-        trainingSet = np.concatenate(list(folds[j] for j in range(0,k) if j != i))
+        trainingSet = np.concatenate(
+            list(folds[j] for j in range(0, k) if j != i))
 
         root, depth, leafCount = decision_tree_learning(trainingSet)
 
         accuracy += evaluate(testSet, root)
 
     return accuracy/k
-        
-
 
     #training_ds= ds[:n,:]
-
