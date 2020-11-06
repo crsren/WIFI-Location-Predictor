487f88c330796976c33fcb8036f190648f4c08a9
nayankad <nk2018@ic.ac.uk>
2020-11-06 16:00:09 +0000

    plotting, depth, and draw member functions

--- main.py
+++ main.py
@@ -3,51 +3,50 @@ import matplotlib.path as mpath
 import matplotlib.lines as mlines
 import matplotlib.patches as mpatches
 from matplotlib.collections import PatchCollection
-from collections import deque
 import numpy as np
 from testing import *
 from training import *
 from node import *
 
-def plot_graph(root, x1, x2, y1, y2, gap, ax):
-    q1 = [(root, x1, x2, y1, y2)]
-    while len(q1) > 0:
-        q2 = q1.pop(0)
-        node, x1, x2, y1, y2 = q2[0], q2[1], q2[2], q2[3], q2[4]
-        a, v, p = node.attribute, node.value, node.pruned
-        text = '['+str(a)+']@'+str(v)  # +'->'+str(p)
-        c = x1+(x2-x1)/2.0
-        d = (c-x1)/2.0
+# def plot_graph(root, x1, x2, y1, y2, gap, ax):
+#     q1 = [(root, x1, x2, y1, y2)]
+#     while len(q1) > 0:
+#         q2 = q1.pop(0)
+#         node, x1, x2, y1, y2 = q2[0], q2[1], q2[2], q2[3], q2[4]
+#         a, v, p = node.attribute, node.value, node.pruned
+#         text = '['+str(a)+']@'+str(v)  # +'->'+str(p)
+#         c = x1+(x2-x1)/2.0
+#         d = (c-x1)/2.0
+#
+#         if node.left is not None:
+#             q1.append((node.left, x1, c, y1, y2-gap))
+#             ax.annotate(text, xy=(c-d, y2-gap),
+#                         xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
+#
+#         if node.right is not None:
+#             q1.append((node.right, c, x2, y1, y2-gap))
+#             ax.annotate(text, xy=(c+d, y2-gap),
+#                         xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
+#
+#         if node.left is None and node.right is None:
+#             an1 = ax.annotate(node.leaf, xy=(c, y2), xycoords="data", va="bottom", ha="center",
+#                               bbox=dict(color="green", boxstyle="circle", fc="w"))
 
-        if node.left is not None:
-            q1.append((node.left, x1, c, y1, y2-gap))
-            ax.annotate(text, xy=(c-d, y2-gap),
-                        xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
-
-        if node.right is not None:
-            q1.append((node.right, c, x2, y1, y2-gap))
-            ax.annotate(text, xy=(c+d, y2-gap),
-                        xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
-
-        if node.left is None and node.right is None:
-            an1 = ax.annotate(node.leaf, xy=(c, y2), xycoords="data", va="bottom", ha="center",
-                              bbox=dict(color="green", boxstyle="circle", fc="w"))
-
-def max_depth(node):
-    if node is None:
-        return 0 ;
-
-    else :
-
-        # Compute the depth of each subtree
-        lDepth = max_depth(node.left)
-        rDepth = max_depth(node.right)
-
-        # Use the larger one
-        if (lDepth > rDepth):
-            return lDepth+1
-        else:
-            return rDepth+1
+# def max_depth(node):
+#     if node is None:
+#         return 0 ;
+#
+#     else :
+#
+#         # Compute the depth of each subtree
+#         lDepth = max_depth(node.left)
+#         rDepth = max_depth(node.right)
+#
+#         # Use the larger one
+#         if (lDepth > rDepth):
+#             return lDepth+1
+#         else:
+#             return rDepth+1
 
 
 def loadClean():
@@ -84,50 +83,52 @@ def main():
     # print(trainingSet)
 
     root, depth, leafCount = decision_tree_learning(trainingSet)
+    pruned = False
+    #md = root.max_depth()
 
-    md = max_depth(root)
+    # fig, ax = plt.subplots(figsize=(1000, 10))
+    # gap = 1.0/depth
+    # plt.axis('off')
+    #plt.title("Unpruned Tree, depth:"+str(md), loc='left')
+    root.draw(pruned)
+    # plot_graph(root, 0.0, 1.0, 0.0, 1.0, gap, ax)
+    # #ax.set_title("Unpruned Tree, depth:"+str(md), align="left")
+    # fig.subplots_adjust(top=0.95)
+    # fig.subplots_adjust(bottom=0.03)
+    # fig.subplots_adjust(left=0.03)
+    # fig.subplots_adjust(right=0.99)
+    # plt.show()
 
-    fig, ax = plt.subplots(figsize=(1000, 10))
-    gap = 1.0/depth
-    plt.axis('off')
-    plt.title("Unpruned Tree, depth:"+str(md), loc='left')
-    plot_graph(root, 0.0, 1.0, 0.0, 1.0, gap, ax)
-    #ax.set_title("Unpruned Tree, depth:"+str(md), align="left")
-    fig.subplots_adjust(top=0.95)
-    fig.subplots_adjust(bottom=0.03)
-    fig.subplots_adjust(left=0.03)
-    fig.subplots_adjust(right=0.99)
-    plt.show()
 
-
-    print("unpruned depth:", md)
+    #print("unpruned depth:", md)
 
     #print(evaluate(testSet, root))
 
     print("Pruning!")
     # # Split into actual validation set later!!!
     root.perfectlyPruned(testSet, root)
-
+    pruned = True
     # avgAccuracy = crossValidate(ds)
     # print("average accuracy: ", avgAccuracy)
     #prune(root, testSet)
 
-    nmd = max_depth(root)
+    #nmd = root.max_depth()
 
-    fig, ax = plt.subplots(figsize=(1000, 10))
-    gap = 1.0/depth
-    plt.axis('off')
-    plt.title("Pruned Tree, depth:"+str(nmd), loc='left')
-    plot_graph(root, 0.0, 1.0, 0.0, 1.0, gap, ax)
-    #ax.set_title("Pruned Tree, depth:"+str(nmd), align="left")
-    fig.subplots_adjust(top=0.95)
-    fig.subplots_adjust(bottom=0.03)
-    fig.subplots_adjust(left=0.03)
-    fig.subplots_adjust(right=0.99)
-    plt.show()
+    # fig, ax = plt.subplots(figsize=(1000, 10))
+    # gap = 1.0/depth
+    # plt.axis('off')
+    #plt.title("Pruned Tree, depth:"+str(nmd), loc='left')
+    root.draw(pruned)
+    # plot_graph(root, 0.0, 1.0, 0.0, 1.0, gap, ax)
+    # #ax.set_title("Pruned Tree, depth:"+str(nmd), align="left")
+    # fig.subplots_adjust(top=0.95)
+    # fig.subplots_adjust(bottom=0.03)
+    # fig.subplots_adjust(left=0.03)
+    # fig.subplots_adjust(right=0.99)
+    # plt.show()
 
 
-    print("pruned depth:", nmd)
+    #print("pruned depth:", nmd)
 
     return  # root, testSet
 
--- node.py
+++ node.py
@@ -1,3 +1,9 @@
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import matplotlib.lines as mlines
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
+
 node = dict(boxstyle="square", fc="w")
 connection = dict(arrowstyle="-")
 
@@ -73,11 +79,58 @@ class Node:
                 print("Didn't prune ", len(self.dataset))
                 return False
 
-    # function to draw tree from this node recursively
-    # def plot(self, width, depth):
-        # a = plt.figure(1,figsize=(8,8))
-        # a.clf()
-        # draw this node
-        # if not a leaf node, draw connections to child nodes
-        # left.plot()
-        # right.plot()
+
+    def max_depth(self):
+        # Compute the depth of each subtree
+        if(self.left == None or self.right == None):
+            return 0
+        else:
+            lDepth = self.left.max_depth()
+            rDepth = self.right.max_depth()
+
+        # Use the larger one
+        if (lDepth > rDepth):
+            return lDepth+1
+        else:
+            return rDepth+1
+
+
+    def plot_graph(self, x1, x2, y1, y2, gap, ax):
+        q1 = [(self, x1, x2, y1, y2)]
+        while len(q1) > 0:
+            q2 = q1.pop(0)
+            node, x1, x2, y1, y2 = q2[0], q2[1], q2[2], q2[3], q2[4]
+            a, v, p = node.attribute, node.value, node.pruned
+            text = '['+str(a)+']@'+str(v)  # +'->'+str(p)
+            c = x1+(x2-x1)/2.0
+            d = (c-x1)/2.0
+
+            if node.left is not None:
+                q1.append((node.left, x1, c, y1, y2-gap))
+                ax.annotate(text, xy=(c-d, y2-gap),
+                            xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
+
+            if node.right is not None:
+                q1.append((node.right, c, x2, y1, y2-gap))
+                ax.annotate(text, xy=(c+d, y2-gap),
+                            xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
+
+            if node.left is None and node.right is None:
+                an1 = ax.annotate(node.leaf, xy=(c, y2), xycoords="data", va="bottom", ha="center",
+                                  bbox=dict(color="green", boxstyle="circle", fc="w"))
+
+    def draw(self, pruned):
+        fig, ax = plt.subplots(figsize=(1000, 10))
+        depth = self.max_depth()
+        gap = 1.0/depth
+        plt.axis('off')
+        if(pruned is True):
+            plt.title("Pruned Tree, depth:"+str(depth), loc='left')
+        else:
+            plt.title("Unpruned Tree, depth:"+str(depth), loc='left')
+        self.plot_graph(0.0, 1.0, 0.0, 1.0, gap, ax)
+        fig.subplots_adjust(top=0.95)
+        fig.subplots_adjust(bottom=0.03)
+        fig.subplots_adjust(left=0.03)
+        fig.subplots_adjust(right=0.99)
+        plt.show()
