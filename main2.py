0204e8020420562b993ab91c1b6ae09ead27f84b
nayankad <nk2018@ic.ac.uk>
2020-11-06 15:24:45 +0000

    Revert "some stuff"
    This reverts commit 8510fcf818f40bda7f498a3fce42db93c3eaf799.

--- main2.py
+++ main2.py
@@ -170,9 +170,6 @@ class Node:
             # prune even if same accuracy since more efficient → >=
             if(post_accuracy >= pre_accuracy):
                 print("Pruned ", len(self.dataset))
-                self.left = None
-                self.right = None
-                self.leaf = 1
                 return True
             else:
                 self.leaf = 0
@@ -327,24 +324,31 @@ def plot_graph(root, x1, x2, y1, y2, gap, ax):
     q1 = [(root, x1, x2, y1, y2)]
     while len(q1) > 0:
         q2 = q1.pop(0)
-        node, x1, x2, y1, y2 = q2[0], q2[1], q2[2], q2[3], q2[4]
-        a, v, p = node.attribute, node.value, node.pruned
+        node = q2[0]
+        x1 = q2[1]
+        x2 = q2[2]
+        y1 = q2[3]
+        y2 = q2[4]
+        a = node.attribute
+        v = node.value
+        p = node.pruned
         text = '['+str(a)+']@'+str(v)  # +'->'+str(p)
-        c = x1+(x2-x1)/2.0
-        d = (c-x1)/2.0
+
+        center = x1+(x2-x1)/2.0
+        d = (center-x1)/2.0
 
         if node.left is not None:
-            q1.append((node.left, x1, c, y1, y2-gap))
-            ax.annotate(text, xy=(c-d, y2-gap),
-                        xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
+            q1.append((node.left, x1, center, y1, y2-gap))
+            ax.annotate(text, xy=(center-d, y2-gap),
+                        xytext=(center, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
 
         if node.right is not None:
-            q1.append((node.right, c, x2, y1, y2-gap))
-            ax.annotate(text, xy=(c+d, y2-gap),
-                        xytext=(c, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
+            q1.append((node.right, center, x2, y1, y2-gap))
+            ax.annotate(text, xy=(center+d, y2-gap),
+                        xytext=(center, y2), arrowprops=dict(arrowstyle="-"), bbox=dict(boxstyle="round", fc="w"))
 
         if node.left is None and node.right is None:
-            an1 = ax.annotate(node.leaf, xy=(c, y2), xycoords="data", va="bottom", ha="center",
+            an1 = ax.annotate(node.leaf, xy=(center, y2), xycoords="data", va="bottom", ha="center",
                               bbox=dict(color="green", boxstyle="circle", fc="w"))
 
 def max_depth(node):
@@ -354,7 +358,7 @@ def max_depth(node):
     else :
 
         # Compute the depth of each subtree
-        lDepth = max_depth(node.left)
+        lDepth = max_depth(node.left) 
         rDepth = max_depth(node.right)
 
         # Use the larger one
@@ -377,14 +381,12 @@ def main():
     clean_ds = loadClean()
     noisy_ds = loadNoisy()
     entire_ds = np.vstack((clean_ds, noisy_ds))
-    #np.random.shuffle(clean_ds)
-    np.random.shuffle(noisy_ds)
-    #np.random.shuffle(entire_ds)
+    np.random.shuffle(entire_ds)
     #confusionAvg = prunedCrossValidate_confusion(entire_ds)
-    #confusionAvg = crossValidate_confusion(entire_ds)
-    #print("Average: ", confusionAvg)
+    confusionAvg = crossValidate_confusion(entire_ds)
+    print("Average: ", confusionAvg)
 
-    lol = np.split(noisy_ds, 20)
+    lol = np.split(ds, 40)
     mini_ds = lol[0]
 
     folds = np.split(mini_ds, 2)
@@ -399,21 +401,16 @@ def main():
 
     root, depth, leafCount = decision_tree_learning(trainingSet)
 
-    md = max_depth(root)
-
     fig, ax = plt.subplots(figsize=(1000, 10))
     gap = 1.0/depth
-    plt.axis('off')
-    plt.title("Unpruned Tree, depth:"+str(md), loc='left')
     plot_graph(root, 0.0, 1.0, 0.0, 1.0, gap, ax)
-    #ax.set_title("Unpruned Tree, depth:"+str(md), align="left")
-    fig.subplots_adjust(top=0.95)
+    fig.subplots_adjust(top=0.98)
     fig.subplots_adjust(bottom=0.03)
     fig.subplots_adjust(left=0.03)
     fig.subplots_adjust(right=0.99)
     plt.show()
 
-
+    md = max_depth(root)
     print("unpruned depth:", md)
 
     #print(evaluate(testSet, root))
@@ -426,22 +423,17 @@ def main():
     # print("average accuracy: ", avgAccuracy)
     #prune(root, testSet)
 
-    nmd = max_depth(root)
-
     fig, ax = plt.subplots(figsize=(1000, 10))
     gap = 1.0/depth
-    plt.axis('off')
-    plt.title("Pruned Tree, depth:"+str(nmd), loc='left')
     plot_graph(root, 0.0, 1.0, 0.0, 1.0, gap, ax)
-    #ax.set_title("Pruned Tree, depth:"+str(nmd), align="left")
-    fig.subplots_adjust(top=0.95)
+    fig.subplots_adjust(top=0.98)
     fig.subplots_adjust(bottom=0.03)
     fig.subplots_adjust(left=0.03)
     fig.subplots_adjust(right=0.99)
     plt.show()
 
-
-    print("pruned depth:", nmd)
+    md = max_depth(root)
+    print("pruned depth:", md)
 
     return  # root, testSet
 
